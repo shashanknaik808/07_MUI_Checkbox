9497075fc3a6247e8e1169bf366b41627e569b60
Shashank Naik <shashanknaik808@gmail.com>
2023-12-04 13:19:24 +0530

    Added a form with name, email, and password fields

--- mui-tut/src/Test.jsx
+++ mui-tut/src/Test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextField, Typography } from '@mui/material';
+import { TextField, Button } from '@mui/material';
 
 function Test() {
 
@@ -7,10 +7,19 @@ function Test() {
 
     return (
         <div>
-            <TextField value={name} onChange={(e) => setName(e.target.value)} type={'text'} sx={{ margin: 3 }} placeholder='Name' variant='outlined' />
-            <TextField type={'email'} sx={{ margin: 3 }} placeholder='Email' variant='standard' />
-            <TextField type={'password'} sx={{ margin: 3 }} placeholder='Password' variant='filled' />
-            <Typography>{name}</Typography>
+            <form onSubmit={handleSubmit}>
+                <TextField name='name' value={inputs.name}
+                    onChange={handleChange} type={'text'}
+                    sx={{ margin: 3 }} placeholder='Name' variant='outlined' />
+                <TextField name='email' value={inputs.email}
+                    onChange={handleChange} type={'email'}
+                    sx={{ margin: 3 }} placeholder='Email' variant='standard' />
+                <TextField name='password' value={inputs.password}
+                    onChange={handleChange} type={'password'}
+                    sx={{ margin: 3 }} placeholder='Password' variant='filled' />
+
+                <Button type='submit'>Submit</Button>
+            </form>
         </div>
     )
 }
